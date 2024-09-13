09f4e9ed526d88253cf34051618bf20f5dc94049
Yym2023 <139544597+Yym2023@users.noreply.github.com>
2024-09-13 22:22:55 +0800

    Add files via upload

---
+++ zyjz.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var resources = document.querySelectorAll('img, audio, link[rel="stylesheet"]')
+    var totalResources = resources.length
+    var loadedResources = 0
+
+    // 更新进度显示函数
+    function updateProgress() {
+        var percentage = Math.round((loadedResources / totalResources) * 100)
+        document.getElementById('progress-text').textContent = `${percentage}%`
+        console.log(percentage + '%')
+        if (loadedResources === totalResources) {
+            document.getElementById('progress-container').style.display = 'none'
+            document.getElementById('app').style.display = 'block'
+        }
+    }
+
+    // 为每个资源添加事件监听器
+    resources.forEach(function (resource) {
+        if (resource.tagName === 'LINK') {
+            resource.onload = resource.onerror = function () {
+                loadedResources++
+                updateProgress()
+            }
+        } else {
+            resource.onload = resource.onerror = function () {
+                loadedResources++
+                updateProgress()
+            }
+        }
+    })
+
+    updateProgress() // 初始化进度为0%
+})
