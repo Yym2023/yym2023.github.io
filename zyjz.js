b3f4571d191078b299f9d9ab0729fcb7b280c78e
Yym2023 <139544597+Yym2023@users.noreply.github.com>
2024-10-07 15:28:28 +0800

    Add files via upload

--- zyjz.js
+++ zyjz.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var resources = document.querySelectorAll('img, audio, link[rel="stylesheet"]')
+    var resources = document.querySelectorAll('img, audio')
     var totalResources = resources.length
-    var loadedResources = 1
+    var loadedResources = 0
     console.log(totalResources)
     console.log(resources)
 
     // 更新进度显示函数
     function updateProgress() {
         var percentage = Math.round((loadedResources / totalResources) * 100)
-        document.getElementById('progress-text').textContent = `${percentage}%`
+        document.getElementById('loading-text1').textContent = `${percentage}%`
+        // 获取并设置 :root {--loading-text:0%；}的值
+        document.documentElement.style.setProperty('--loading-text', `${percentage}%`)
         console.log(percentage + '%')
         if (loadedResources === totalResources) {
-            document.getElementById('progress-container').style.display = 'none'
+            document.getElementById('loading').style.display = 'none'
             document.getElementById('app').style.display = 'block'
         }
     }
